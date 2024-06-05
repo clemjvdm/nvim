9c7afb884930e204e9bcb5627774ff41bb71d48b
clement <clement.jvdm@gmail.com>
2024-06-05 20:37:12 +0200

    improved telescope config

--- lua/clement/plugins/telescope.lua
+++ lua/clement/plugins/telescope.lua
@@ -1,6 +1,25 @@
 return {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim', { "nvim-telescope/telescope-fzf-native.nvim",build="make" }, "nvim-tree/nvim-web-devicons"},
+
+    config = function ()
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
+
+        telescope.setup({
+            defaults = {
+                path_display = {"smart"},
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_next,
+                        ["<C-l>"] = actions.move_selection_previous,
+                    },
+                },
+            },
+        })
+
+    telescope.load_extension("fzf")
+    end,
 
 }
